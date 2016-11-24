5ab6ec2a35a488efe1b74e11bfce29f331f3c9e9
licho <licho.sun@gmail.com>
2016-11-24 19:36:51 +0800

    bbd 增加page_size

--- app/datasource/query_enterprise.py
+++ app/datasource/query_enterprise.py
@@ -10,11 +10,13 @@ class QueryEnterprise():
 
     @classmethod
     def query(cls, *args, **kwargs):
-        key = kwargs.get['key']
-        index = kwargs.get['index']
-        BBD.query(enterprise_name=key)
+        key = kwargs.get('key')
+        index = kwargs.get('index')
+        page_size = kwargs.get('page_size')
+        # BBD.query(enterprise_name=key, page_size=page_size)
+
 
     @classmethod
-    def company_list(cls, query):
-        result = BBD.query_companysearch2(query=query)
-        return result
+    def company_list(cls, query, page_size=None):
+        result = BBD.query_companysearch2(query=query, page_size=page_size)
+        return result.text
