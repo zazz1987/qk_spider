84337d95667919e2ab635531e974897515dd3ff9
listener <1045870696@qq.com>
2016-11-24 18:34:29 +0800

    查询其他信息
    行政处罚,招聘数据,招中标信息,境外投资

--- app/datasource/query_enterprise.py
+++ app/datasource/query_enterprise.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import json
+
 from queue import Queue
 
 from app.datasource.bbd.bbd import BBD
@@ -12,3 +14,16 @@ def QueryEnterprise():
         key = kwargs.get['key']
         index = kwargs.get['index']
         BBD.query(enterprise_name=key)
+
+        result = {}
+        #其他
+        if index == 7:
+            #行政处罚
+            result['bbd_ent_admin_pena'] = BBD.query_ent_admin_pena(company=key, **kwargs)
+            #招聘数据
+            result['bbd_recruit'] = BBD.query_recruit(company=key, **kwargs)
+            #招中标信息
+            result['bbd_bidwinner'] = BBD.query_bidwinner(company=key, **kwargs)
+            #境外投资
+            result['bbd_overseasinv'] = BBD.query_overseasinv(company=key, **kwargs)
+        return json.dumps(result)
